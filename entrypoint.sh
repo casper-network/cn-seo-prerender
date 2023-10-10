84e16ddc3be51b5df0f4a8ed01a2dbd3cd183f6e
Sandro Ducceschi <ducceschi@ditoy.com>
2023-10-10 09:39:01 +0200

    MODIFIED: disable shell started chrome instance

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 --hide-scrollbars --blink-settings=imagesEnabled=false --no-sandbox --disable-dev-shm-usage &
+# google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 --hide-scrollbars --blink-settings=imagesEnabled=false --no-sandbox --disable-dev-shm-usage &
 #block access to AWS metadata IP before starting server
 ip route add blackhole 169.254.169.254
 pm2-runtime index.js
